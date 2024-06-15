13c4ddd96756baffffaf2308e564ded302b4f89b
Aditya <sharmaditya5150@gmail.com>
2024-06-16 03:39:21 +0530

    Reduceing state management : removing unneccesary state

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,25 @@ import GameBoard from "./components/GameBoard"
 import Player from "./components/Player"
 import Log from "./components/Log"
 
+function deriveActivePlayer(gameTurns){
+  let currentPlayer = 'X'
+  if(gameTurns.length > 0 && gameTurns[0].player == 'X'){
+    currentPlayer = 'O'
+  }else{
+    currentPlayer = 'X'
+  }
+  return currentPlayer
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([])
-  const [activePlayer, setActivePlayer] = useState('X')
-
+  // const [activePlayer, setActivePlayer] = useState('X')
+  const activePlayer = deriveActivePlayer(gameTurns)
+  
   function handleSelectSquare(rowIndex, colIndex){
-    setActivePlayer((currActivePlayer)=>currActivePlayer==='X'?'O':'X')
+    // setActivePlayer((currActivePlayer)=>currActivePlayer==='X'?'O':'X')
     setGameTurns(prevTurns => {
-      let currentPlayer = 'X'
-      if(prevTurns.length > 0 && prevTurns[0].player === 'X'){
-        currentPlayer = 'O'
-      }else{
-        currentPlayer = 'X'
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns)
       const updatedTurns = [{square : {row:rowIndex, col:colIndex}, player: activePlayer},...prevTurns]
       return updatedTurns
     })
